bd97d1c145aa6f3821623764a696b15067d1d1cd
tawchifulislam <tawchif04@gmail.com>
2026-06-11 16:49:18 +0600

    Solve LeetCode 21

--- index.js
+++ index.js
@@ -94,3 +94,26 @@ var isValid = function (s) {
 
   return stack.length === 0;
 };
+
+// LeetCode 21. Merge Two Sorted Lists
+let mergeTwoLists = function (list1, list2) {
+  let dummy = new ListNode(0);
+  let current = dummy;
+  while (list1 && list2) {
+    if (list1.val < list2.val) {
+      current.next = list1;
+      list1 = list1.next;
+    } else {
+      current.next = list2;
+      list2 = list2.next;
+    }
+    current = current.next;
+  }
+  if (list1) {
+    current.next = list1;
+  }
+  if (list2) {
+    current.next = list2;
+  }
+  return dummy.next;
+};
