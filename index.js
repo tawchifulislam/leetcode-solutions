d546455eaa396de7f3060d17de1ce1ae456245fe
tawchifulislam <tawchif04@gmail.com>
2026-06-11 16:17:30 +0600

    Solve LeetCode 13

--- index.js
+++ index.js
@@ -40,3 +40,33 @@ var firstUniqChar = function (s) {
 
   return -1;
 };
+
+// LeetCode 13. Roman to Integer
+
+const symbols = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000,
+};
+
+var romanToInt = function (s) {
+  let total = 0;
+  for (let i = 0; i < s.length; i++) {
+    let currentSymbol = s[i];
+    let currentValue = symbols[currentSymbol];
+    let nextSymbol = s[i + 1];
+    let nextValue = symbols[nextSymbol];
+
+    if (currentValue < nextValue) {
+      total += nextValue - currentValue;
+      i++;
+    } else {
+      total += currentValue;
+    }
+  }
+  return total;
+};
