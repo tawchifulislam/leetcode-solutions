fc31f4c53a3464cbead6588195845e503e4535a1
tawchifulislam <tawchif04@gmail.com>
2026-06-11 16:33:35 +0600

    Solve LeetCode 20

--- index.js
+++ index.js
@@ -70,3 +70,27 @@ var romanToInt = function (s) {
   }
   return total;
 };
+
+// LeetCode 20. Valid Parentheses
+var isValid = function (s) {
+  let stack = [];
+  let pairs = {
+    '(': ')',
+    '{': '}',
+    '[': ']',
+  };
+
+  for (let i = 0; i < s.length; i++) {
+    let currentChar = s[i];
+    if (pairs[currentChar]) {
+      stack.push(currentChar);
+    } else {
+      let lastChar = stack.pop();
+      if (currentChar !== pairs[lastChar]) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+};
