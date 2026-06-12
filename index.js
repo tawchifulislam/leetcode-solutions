3a93201c9d876a292cdabc2b3faaf0d64ad0fa23
tawchifulislam <tawchif04@gmail.com>
2026-06-12 12:00:49 +0600

    Solve LeetCode 26

--- index.js
+++ index.js
@@ -117,3 +117,16 @@ let mergeTwoLists = function (list1, list2) {
   }
   return dummy.next;
 };
+
+// LeetCode 26. Remove Duplicates from Sorted Array
+var removeDuplicates = function (nums) {
+  if (nums.length === 0) return 0;
+  let i = 0;
+  for (let j = 1; j < nums.length; j++) {
+    if (nums[j] !== nums[i]) {
+      i++;
+      nums[i] = nums[j];
+    }
+  }
+  return i + 1;
+};
