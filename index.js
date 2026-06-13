905d2d7c9d1221468e77afb54019430590784c09
tawchifulislam <tawchif04@gmail.com>
2026-06-13 16:25:52 +0600

    Solve LeetCode 66

--- index.js
+++ index.js
@@ -166,3 +166,16 @@ var lengthOfLastWord = function (s) {
   let words = s.split(' ');
   return words[words.length - 1].length;
 };
+
+// LeetCode 66. Plus One
+var plusOne = function (digits) {
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] < 9) {
+      digits[i]++;
+      return digits;
+    }
+    digits[i] = 0;
+  }
+  digits.unshift(1);
+  return digits;
+};
