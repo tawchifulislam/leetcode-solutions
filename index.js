52b8ca4fb79c89fdf4555b0b071d5e2c7b713c2c
tawchifulislam <tawchif04@gmail.com>
2026-06-15 16:25:37 +0600

    solve leetcode 9

--- index.js
+++ index.js
@@ -224,3 +224,18 @@ var longestCommonPrefix = function (strs) {
 
   return prefix;
 };
+
+// LeetCode 9. Palindrome Number
+var isPalindrome = function (x) {
+  if (x < 0) return false;
+  let str = x.toString();
+  let left = 0,
+    right = str.length - 1;
+  while (left < right) {
+    if (str[left] !== str[right]) return false;
+    left++;
+    right--;
+  }
+
+  return true;
+};
