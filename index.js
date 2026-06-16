0d4693a426973b9e2f99ac81722f620f4eb713dd
tawchifulislam <tawchif04@gmail.com>
2026-06-16 09:22:29 +0600

    Solve LeetCode 2634

--- index.js
+++ index.js
@@ -248,3 +248,14 @@ var map = function (arr, fn) {
   }
   return result;
 };
+
+// LeetCode 2634. Filter Elements from Array
+var filter = function (arr, fn) {
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (fn(arr[i], i)) {
+      result.push(arr[i]);
+    }
+  }
+  return result;
+};
