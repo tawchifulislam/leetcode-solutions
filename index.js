5f72815202b2af2a1f1eccc642c08404c0ae293e
tawchifulislam <tawchif04@gmail.com>
2026-06-16 15:58:14 +0600

    Solve LeetCode 2695

--- index.js
+++ index.js
@@ -268,3 +268,13 @@ var reduce = function (arr, fn, init) {
   }
   return acc;
 };
+// LeetCode 2695. Array Wrapper
+var ArrayWrapper = function (nums) {
+  this.nums = nums;
+};
+ArrayWrapper.prototype.valueOf = function () {
+  return this.nums.reduce((acc, n) => acc + n, 0);
+};
+ArrayWrapper.prototype.toString = function () {
+  return `[${this.nums.join(',')}]`;
+};
